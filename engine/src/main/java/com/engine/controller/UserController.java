f532c2691a0f75700569bdbdd5731ced1c964ddb
����ɽ <544610616@qq.com>
2019-05-01 15:56:17 +0800

    userController

--- engine/src/main/java/com/engine/controller/UserController.java
+++ engine/src/main/java/com/engine/controller/UserController.java
@@ -3,13 +3,12 @@ package com.engine.controller;
 import com.engine.model.User;
 import com.engine.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(value = "/user")
+@RestController
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     UserService userService;
