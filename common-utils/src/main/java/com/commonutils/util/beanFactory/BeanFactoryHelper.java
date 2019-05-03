97dd122da71fb34879bbd5525d0991a46f57f6e9
����ɽ <544610616@qq.com>
2019-05-03 15:30:48 +0800

    jdbc依赖

---
+++ common-utils/src/main/java/com/commonutils/util/beanFactory/BeanFactoryHelper.java
@@ -0,0 +1,33 @@
+package com.commonutils.util.beanFactory;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BeanFactoryHelper implements ApplicationContextAware
+{
+	private static ApplicationContext applicationContext;
+	
+	private BeanFactoryHelper(){}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		BeanFactoryHelper.applicationContext = applicationContext;
+	}
+
+	public static BeanFactory getBeanfactory() {
+		return applicationContext;
+	}
+	
+	public static Object getBean(String serviceName)
+	{
+		if(applicationContext == null)
+		{
+			getBeanfactory();
+		}
+		return applicationContext.getBean(serviceName);
+	}
+}
