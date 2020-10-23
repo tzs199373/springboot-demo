1dfbbbe32db0b34420c8b5405018a17a5be37056
����ɽ <544610616@qq.com>
2020-10-23 10:24:35 +0800

    统计请求数

--- web/src/main/java/com/web/scheduled/PrintTask.java
+++ web/src/main/java/com/web/scheduled/PrintTask.java
@@ -8,12 +8,9 @@ import java.util.Date;
 @Component
 public class PrintTask {
 
-    /**
-     * ��һ�����ÿ�ʼ���ٴε��õ���ʱ�����õȴ��ϴε������
-     */
 //    @Scheduled(fixedDelay = 3000*1,initialDelay=1000*10)
     public void fixedDelay() throws InterruptedException {
         Thread.sleep(2000);
-        System.out.println("ִ�в���fixedRateʱ�䣺"+new Date(System.currentTimeMillis()));
+        System.out.println("fixedDelay:"+new Date(System.currentTimeMillis()));
     }
 }
