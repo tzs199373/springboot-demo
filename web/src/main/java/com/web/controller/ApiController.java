308b4ee0645ca4f3a524906e929b9d42f1ca4a69
����ɽ <544610616@qq.com>
2020-10-29 23:40:14 +0800

    统计请求数

---
+++ web/src/main/java/com/web/controller/ApiController.java
@@ -0,0 +1,29 @@
+package com.web.controller;
+
+import com.web.model.Person;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+//Api注解，描述信息 可通过tag进行分类
+@Api(value = "ApiController", description = "ApiController")
+public class ApiController {
+    @PostMapping("/addPerson")
+    //方法描述
+    @ApiOperation(notes = "添加人员", value = "addPerson")
+    public Person addPerson(
+            @ApiParam(name = "name", value = "姓名") @RequestParam("name") String name,
+            @ApiParam(name = "age", value = "年龄")  @RequestParam("age") Integer age) {
+        Person person = new Person();
+        person.setAge(age);
+        person.setName(name);
+
+        return person;
+    }
+}
