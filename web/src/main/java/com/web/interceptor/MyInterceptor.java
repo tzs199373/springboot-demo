104ef87eae34b88d8e3db8d6f1ae9a56813d2025
����ɽ <544610616@qq.com>
2020-11-03 09:13:44 +0800

    1

---
+++ web/src/main/java/com/web/interceptor/MyInterceptor.java
@@ -0,0 +1,22 @@
+package com.web.interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("request«Î«Ûµÿ÷∑path["+request.getServletPath()+"] uri["+request.getRequestURI()+"]");
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+
+}
